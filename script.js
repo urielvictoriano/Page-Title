d2e9de421731440698e483b62a2e10ec675dca1a
urielvictoriano <43676075+urielvictoriano@users.noreply.github.com>
2019-03-05 09:24:21 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("button").click(function(){
+    var name = $(".name").val();
+    var Question = $(".Question").val();
+    var one = $(".sport").val();
+    
+    console.log(name);
+    console.log(Question);
+    console.log(one);
+});
